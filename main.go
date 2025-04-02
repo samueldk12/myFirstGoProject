02179b0dca657107ccf78b072e681ed3cbfa63b8
Samuel Arao <samuel.arao@gmail.com>
2025-04-01 21:48:11 -0300

    feat: finalizacao projeto de advinha

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand/v2"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
+	fmt.Println("Jogo da advinhação")
+	fmt.Println("Um número aleatório será sorteado. Tente acertar.O número é um inteiro entre 0 e 100")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	chutes := [10]int64{}
+	x := rand.Int64N(101)
+
+	for i := range chutes {
+		fmt.Println("Qual é o seu chute?")
+		scanner.Scan()
+		chute := scanner.Text()
+		chute = strings.TrimSpace(chute)
+
+		chuteInt, err := strconv.ParseInt(chute, 10, 64)
+
+	
+		if err != nil{
+			fmt.Println("O seu chute tem que ser um número inteiro.")
+			return
+		}
+
+		switch {
+		case chuteInt < x:
+			fmt.Println("Você errou. O número sorteado é maior que", chuteInt)
+		case chuteInt > x:
+			fmt.Println("Você errou. O número sorteado é menor que", chuteInt)
+		case chuteInt == x:
+			fmt.Printf(
+				"Parabéns! Você acertou! O número era: %d\n"+
+				"Você acertou em %d tentativas.\n"+
+				"Essas foram as suas tentativas: %v\n", 
+			x, i+1 , chutes[:i])		
+		}
+
+		chutes[i] = chuteInt
+		
+	}
+
+	fmt.Printf(
+		"Infelizmente você não acertou o número que era: %d\n"+
+		"Você teve 10 tentativas.\n"+
+		"Essas foram as suas tentativas: %v\n", 
+	x, chutes)
+	
 }
